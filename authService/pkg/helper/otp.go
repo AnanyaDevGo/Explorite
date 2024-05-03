2f3e6f06728cd7dec6ee96c15aa337aeb15240e0
Ananya E P <areduvail@gmail.com>
2024-05-03 18:46:34 +0530

    updated

---
+++ authService/pkg/helper/otp.go
@@ -0,0 +1,73 @@
+package helper
+
+import (
+	config "authservice/pkg/conifg"
+	"fmt"
+	"net/smtp"
+)
+
+type SmtpCredentials struct {
+	SmtpConfig config.Smtp
+}
+
+var smtpCredentials SmtpCredentials
+
+func SmtpConfigsForEmailOtp(smtpConfigs config.Smtp) {
+	smtpCredentials.SmtpConfig = smtpConfigs
+}
+
+func SendVerificationEmailWithOtp(otp int, recieverEmail string, recieverName string) error {
+
+	from := smtpCredentials.SmtpConfig.SmtpSender
+	password := smtpCredentials.SmtpConfig.SmtpPassword
+	to := []string{recieverEmail}
+	smtpHost := smtpCredentials.SmtpConfig.SmtpHost
+	smtpPort := smtpCredentials.SmtpConfig.SmtpPort
+
+	subject := "Verify Your Email Address for Explorite"
+	body := fmt.Sprintf("Hello,%s\n\nThank you for signing up for Explorite. To complete your registration and ensure the security of your account, please verify your email address by entering the One-Time Password (OTP) provided below:\n\nOTP: %d\n\nPlease use the OTP to verify your email address on our platform within the next 10 minutes. After this time, the OTP will expire, and you will need to request a new one.\n\nIf you did not request this verification, please disregard this email.\n\nIf you need any assistance or have questions, feel free to reach out to our support team at support@example.com.\n\nThank you for choosing Explorite.\n\nBest regards,\nThe Explorite Team", recieverName, otp)
+	message := []byte("Subject: " + subject + "\r\n" +
+		"\r\n" +
+		body)
+
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+
+	// Send actual message
+
+	fmt.Println("pritntttt", smtpHost)
+	fmt.Println("pritntttt", smtpPort)
+	fmt.Println("pritntttt", auth)
+	fmt.Println("pritntttt", from)
+	fmt.Println("pritntttt", to)
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+	if err != nil {
+		fmt.Println("-----", err)
+		return err
+	}
+	return nil
+}
+
+func SendRestPasswordEmailOtp(otp int, recieverEmail string) error {
+	from := smtpCredentials.SmtpConfig.SmtpSender
+	password := smtpCredentials.SmtpConfig.SmtpPassword
+	to := []string{recieverEmail}
+	smtpHost := smtpCredentials.SmtpConfig.SmtpHost
+	smtpPort := smtpCredentials.SmtpConfig.SmtpPort
+
+	subject := "Reset Your Password"
+	body := fmt.Sprintf("Dear %s,\n\nYou recently requested to reset your password for your Explorite account. To complete the process, please use the following One-Time Password (OTP):\n\nOTP: %d\n\nThis OTP is valid for 10 minutes. Please do not share this OTP with anyone for security reasons. If you did not request a password reset, please ignore this email.\n\nThank you,\nThe Explorite Team", recieverEmail, otp)
+
+	message := []byte("Subject: " + subject + "\r\n" +
+		"\r\n" +
+		body)
+
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+
+	// Send actual message
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+	if err != nil {
+		fmt.Println("-----", err)
+		return err
+	}
+	return nil
+}
