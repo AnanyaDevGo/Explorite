13852c2498213c28025f4bcb524ace2dae991078
Ananya E P <areduvail@gmail.com>
2024-06-20 20:30:15 +0530

    updated notification svc

--- authService/pkg/conifg/config.go
+++ authService/pkg/conifg/config.go
@@ -5,19 +5,20 @@ import (
 )
 
 type Config struct {
-	Port          string `mapstructure:"PORT"`
-	DBHost        string `mapstructure:"DB_HOST"`
-	DBName        string `mapstructure:"DB_NAME"`
-	DBUser        string `mapstructure:"DB_USER"`
-	DBPassword    string `mapstructure:"DB_PASSWORD"`
-	DBPort        string `mapstructure:"DB_PORT"`
-	ExploriteChat string `mapstructure:"Explorite_chat"`
-	KafkaBrokers  string `mapstructure:"KAFKA_BROKERS"`
-	KafkaTopic    string `mapstructure:"KAFKA_TOPIC"`
+	Port                   string `mapstructure:"PORT"`
+	DBHost                 string `mapstructure:"DB_HOST"`
+	DBName                 string `mapstructure:"DB_NAME"`
+	DBUser                 string `mapstructure:"DB_USER"`
+	DBPassword             string `mapstructure:"DB_PASSWORD"`
+	DBPort                 string `mapstructure:"DB_PORT"`
+	ExploriteChat          string `mapstructure:"Explorite_chat"`
+	Explorite_notification string `mapstructure:"Explorite_notification"`
+	KafkaBrokers           string `mapstructure:"KAFKA_BROKERS"`
+	KafkaTopic             string `mapstructure:"KAFKA_TOPIC"`
 }
 
 var envs = []string{
-	"PORT", "DB_HOST", "DB_NAME", "DB_USER", "DB_PASSWORD", "DB_PORT", "Explorite_chat", "KAFKA_BROKERS", "KAFKA_TOPIC",
+	"PORT", "DB_HOST", "DB_NAME", "DB_USER", "DB_PASSWORD", "DB_PORT", "Explorite_chat","Explorite_notification", "KAFKA_BROKERS", "KAFKA_TOPIC",
 }
 
 func LoadConfig() (Config, error) {
