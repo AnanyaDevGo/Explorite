9d71380200d97802fb4d4393910001f087147cea
Ananya E P <areduvail@gmail.com>
2024-06-20 11:59:08 +0530

    updated notification service

--- api-gateway/pkg/api/handler/notification.go
+++ api-gateway/pkg/api/handler/notification.go
@@ -1,12 +1,41 @@
 package handler
 
-import interfaces "ExploriteGateway/pkg/client/interface"
+import (
+	interfaces "ExploriteGateway/pkg/client/interface"
+	"ExploriteGateway/pkg/utils/models"
+	"ExploriteGateway/pkg/utils/response"
+	"net/http"
 
-type NotificationHandler struct{
+	"github.com/gin-gonic/gin"
+)
+
+type NotificationHandler struct {
 	GRPC_Client interfaces.NotificationClient
 }
+
 func NewNotificationHandler(notificationClient interfaces.NotificationClient) *NotificationHandler {
 	return &NotificationHandler{
 		GRPC_Client: notificationClient,
 	}
+}
+
+func (ad *NotificationHandler) GetNotification(c *gin.Context) {
+	var notificationRequest models.NotificationPagination
+	if err := c.ShouldBindJSON(&notificationRequest); err != nil {
+		errorres := response.ClientResponse(http.StatusBadRequest, "details give in wrong format", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorres)
+		return
+	}
+
+	id_string, _ := c.Get("id")
+	id, _ := id_string.(int)
+
+	result, errs := ad.GRPC_Client.GetNotification(id, notificationRequest)
+	if errs != nil {
+		errss := response.ClientResponse(http.StatusBadRequest, "error in getting notification", nil, errs.Error())
+		c.JSON(http.StatusBadRequest, errss)
+		return
+	}
+	succesres := response.ClientResponse(http.StatusOK, "successfully got all notification", result, nil)
+	c.JSON(http.StatusOK, succesres)
 }
